a29a029129b62781bb7c2eea5cb0d561b58fb652
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-08-29 12:06:11 +0200

    done done

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+
+
+
+        public int age { get; set; }
+
+        public string yesmynameis { get; set; }
+
+    }
+}
