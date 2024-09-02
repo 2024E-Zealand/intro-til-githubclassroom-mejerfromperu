eb0f0fc7126bba26a89bb17cb5c569361667d3f0
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-09-02 12:57:39 +0200

    classroom opg

--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MyFirstGitHubClassRoomConsoleApp60;
+
 Console.WriteLine("Hello, World!");
+
+
+student student1 = new student("jim", 5, 23);
+student student2 = new student("jhonny", 2, 23);
+student student3 = new student("jimmy", 3, 22);
+
+// Add students to the list
+List<student> students = new List<student> { student1, student2, student3 };
+
+
+ClassRoom classRoom = new ClassRoom("Teknik", students, DateTime.Now);
+
+Console.WriteLine(classRoom);
+
+Console.WriteLine(student1.Season());
---
+++ MyFirstGitHubClassRoomConsoleApp60/student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class student
+    {
+
+        public student(string name, int birthMonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            Birthday = birthday;
+        }
+
+        public string Name { get; set; }
+
+        public int BirthMonth { get; set; }
+
+        public int Birthday { get; set; }
+
+
+
+            
+        public string Season()
+        {
+            if (BirthMonth == 12 || BirthMonth == 1 || BirthMonth == 2)
+            {
+                return "Winter";
+            }
+            else if (BirthMonth >= 3 && BirthMonth <= 5)
+            {
+                return "Spring";
+            }
+            else if (BirthMonth >= 6 && BirthMonth <= 8)
+            {
+                return "Summer";
+            }
+            else if (BirthMonth >= 9 && BirthMonth <= 11)
+            {
+                return "Autumn";
+            }
+            else
+            {
+                return "Erorr";
+            }
+        }
+    }
+}
+
